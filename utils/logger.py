c64bfc3b10806fd3e8425d078f05214ac95157f2
Manish Kapal <manishhkapal@gmail.com>
2026-06-20 00:30:08 +0530

    Adding Logger file to get more reada

---
+++ utils/logger.py
@@ -0,0 +1,21 @@
+import logging
+import os
+
+os.makedirs("logs", exist_ok=True)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=(
+        "%(asctime)s | "
+        "%(levelname)s | "
+        "%(filename)s:%(lineno)d | "
+        "%(funcName)s | "
+        "%(message)s"
+    ),
+    handlers=[
+        logging.FileHandler("logs/app.log"),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger("whatsapp_bot")
